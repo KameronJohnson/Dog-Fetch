60052f7583db3df801c10402c0595c49f99518e8
kameron johnson <kameronjohnson@gmail.com>
2018-05-07 16:37:33 -0700

    add event listeners to select options and div.card to show random dog images

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@ const select = document.getElementById('breeds');
 const card = document.querySelector('.card');
 const form = document.querySelector('form');
 
+function fetchData(url) {
+  return fetch(url)
+    .then(res => res.json());
+}
 //random dog images
 fetch('https://dog.ceo/api/breeds/image/random')
   .then(response => response.json())
@@ -12,6 +16,19 @@ fetch('https://dog.ceo/api/breeds/list')
   .then(response => response.json())
   .then(data => generateBreedList(data.message));
 
+//fetch breed image
+const fetchBreedImage = () => {
+  const breed = select.value;
+  const img = card.querySelector('img');
+  const paragraph = card.querySelector('p');
+
+  fetchData(`https://dog.ceo/api/breed/${breed}/images/random`)
+    .then(data => {
+      img.src = data.message;
+      img.alt = breed;
+      paragraph.textContent = `Click to view more ${breed}s`
+    });
+}
 
 const generateImage = url => {
   const html = `
@@ -27,3 +44,6 @@ const generateBreedList = data => {
   `).join('');
   select.innerHTML = options;
 }
+
+select.addEventListener('change', fetchBreedImage);
+card.addEventListener('click', fetchBreedImage);
